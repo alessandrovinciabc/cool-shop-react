fc268f469d2547ad4e0fdebe0f0d27869a96f7dc
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-22 23:31:36 +0200

    Link shop button in home to shop page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './Hero.css';
 
 function Hero(props) {
@@ -7,9 +9,11 @@ function Hero(props) {
     <div className="Hero">
       <h2 className="Hero__title">{props.title}</h2>
       <h3 className="Hero__subtitle">{props.subtitle}</h3>
-      <button className="Hero__cta" onClick={props?.button?.onClick}>
-        {props?.button?.text}
-      </button>
+      <Link to="/shop">
+        <button className="Hero__cta" onClick={props?.button?.onClick}>
+          {props?.button?.text}
+        </button>
+      </Link>
     </div>
   );
 }
